fd766a59bc99be6fd31977e05d42940f37088ff3
Anton Iovbak <A.Iovbak@osmp.kz>
2016-07-21 16:24:48 +0600

    bio object added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,5 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 $("#main").append('Anton')
+
+var bio = {'name' : 'Anton Iovbak', 'role' : 'Web Developer', 'contact_info' : ['antoniovbak14@gmail.com', 'GitHub account = AInsolence'], 'picture_url' : 'https://', 'welcome_message' : 'Hello! Nice to meet you here dudes!', 'skills' : []'}
