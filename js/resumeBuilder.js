bc16dfb31db4bff29648a0480573eb541f8f76cb
Anton Iovbak <A.Iovbak@osmp.kz>
2016-07-22 14:06:31 +0600

    practice with objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,6 +2,30 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$("#main").append('Anton')
+//$("#main").append('Anton')
 
-var bio = {'name' : 'Anton Iovbak', 'role' : 'Web Developer', 'contact_info' : ['antoniovbak14@gmail.com', 'GitHub account = AInsolence'], 'picture_url' : 'https://', 'welcome_message' : 'Hello! Nice to meet you here dudes!', 'skills' : []'}
+var bio = {'name' : 'Anton Iovbak', 'role' : 'Web Developer', 
+		'contacts' : {'email' : 'antoniovbak14@gmail.com', 'phone number' : '+7 777 002 44 35',
+		'GitHub account' : 'AInsolence', 'location' : 'Almaty'}, 
+		'welcome_message' : 'Hello! Nice to meet you here dudes!', 
+		'skills' : ['Python', 'JavaScript', 'HTML', 'CSS', 'Postgre SQL', 'Linux', 'GIT'],
+		'bioPic' : 'https://'}
+
+
+var formattedName = HTMLheaderName.replace('%data%', bio['name']);
+
+$("#header").append(formattedName);
+
+var work = {};
+work.position = 'Web Developer';
+work.employer = 'Udacity';
+work.years_work = '1';
+work.city = 'San Francisco';
+
+var education = {};
+education['school'] = 'Udacity online university';
+education['years_attend'] = '1';
+
+$("#main").prepend(work['position']);
+$("#main").prepend(' ');
+$("#main").prepend(education.school);
